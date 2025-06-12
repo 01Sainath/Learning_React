0f61df0ce49e450341a8ecbc4d9a12b4116b16f9
Ibitwar Sainath <ibitwarsainath99@gmail.com>
2025-06-12 18:59:52 +0530

    01basicreact and 01vitereact commited

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <h1>Hello world, Im learning reactjs!!</h1>
+  );
+}
+
+function App1(){
+  return (
+    <h1>Hello world, App1</h1>
+  );
+}
+
+export default App1;
