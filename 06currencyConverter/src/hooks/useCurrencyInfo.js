2adf305ce6614cf8156e50e25095269eb36a8331
01sainath <ibitwarsainath99@gmail.com>
2025-06-24 00:09:50 +0530

    06currencyConverter completed

--- 06currencyConverter/src/hooks/useCurrencyInfo.js
+++ 06currencyConverter/src/hooks/useCurrencyInfo.js
@@ -4,10 +4,10 @@ function useCurrencyInfo(currency){
 
     const [data, setData] =useState({})
     useEffect(()=>{
-        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/
-            currency-api@latest/v1/currencies/usd.json`)
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
             .then((res)=> res.json())
-            .then((res)=>setData(res[{currency}]))
+            .then((res)=>setData(res[currency]))
+            console.log("currency data: ");
             console.log(data);
     }, [currency])
     
